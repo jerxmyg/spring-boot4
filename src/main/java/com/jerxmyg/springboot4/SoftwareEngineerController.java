10ecb4ad863abd12288676011f4f497b7aba807d
Jeremy <32397605+jerxmyg@users.noreply.github.com>
2026-07-08 22:37:12 -0400

    Connected the controller to the service layer

--- src/main/java/com/jerxmyg/springboot4/SoftwareEngineerController.java
+++ src/main/java/com/jerxmyg/springboot4/SoftwareEngineerController.java
@@ -10,14 +10,13 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
-        return List.of(
-                new SoftwareEngineer(
-                        1,
-                        "James",
-                        "java, spring, springboot"
-                )
-        );
+        return softwareEngineerService.getAllSoftwareEngineers();
     }
 }
---
+++ src/main/java/com/jerxmyg/springboot4/SoftwareEngineerRepository.java
@@ -0,0 +1,6 @@
+package com.jerxmyg.springboot4;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+}
