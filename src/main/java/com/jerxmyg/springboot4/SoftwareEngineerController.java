98909c7ad2b47ee5bc14975f530a199d6e906d54
Jeremy <32397605+jerxmyg@users.noreply.github.com>
2026-07-09 09:02:05 -0400

    Completed the proj, finalized the implementation of postgres via docker. Test HTTP requests were successful.

--- src/main/java/com/jerxmyg/springboot4/SoftwareEngineerController.java
+++ src/main/java/com/jerxmyg/springboot4/SoftwareEngineerController.java
@@ -1,9 +1,6 @@
 package com.jerxmyg.springboot4;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +15,15 @@ public class SoftwareEngineerController {
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
         return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
     }
 }
--- src/main/java/com/jerxmyg/springboot4/SoftwareEngineerService.java
+++ src/main/java/com/jerxmyg/springboot4/SoftwareEngineerService.java
@@ -15,4 +15,14 @@ public class SoftwareEngineerService {
     public List<SoftwareEngineer> getAllSoftwareEngineers() {
         return softwareEngineerRepository.findAll();
     }
+
+    public void insertSoftwareEngineer(SoftwareEngineer softwareEngineer) {
+        softwareEngineerRepository.save(softwareEngineer);
+    }
+
+    public SoftwareEngineer getSoftwareEngineerById(Integer id) {
+        return softwareEngineerRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException(
+                        id + "not found"));
+    }
 }
